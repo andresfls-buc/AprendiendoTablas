1466ba557e67e9095580f7d70d248c3dfba1e62e
andresfls-buc <andresflsxx@gmail.com>
2025-06-12 12:06:31 +0900

    Initial Commit

---
+++ AprendiendoTablasTests/AprendiendoTablasTests.swift
@@ -0,0 +1,17 @@
+//
+//  AprendiendoTablasTests.swift
+//  AprendiendoTablasTests
+//
+//  Created by Andres Landazabal on 2025/06/12.
+//
+
+import Testing
+@testable import AprendiendoTablas
+
+struct AprendiendoTablasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
