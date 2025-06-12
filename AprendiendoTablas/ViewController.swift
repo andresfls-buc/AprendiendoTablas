bcc3dfe1cad2c647708f82a05af5f76e77ae0a55
andresfls-buc <andresflsxx@gmail.com>
2025-06-12 12:55:25 +0900

    Learning about tables in StoryBoard

--- AprendiendoTablas/ViewController.swift
+++ AprendiendoTablas/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// MARK: -Referencia de la tabla
+    @IBOutlet weak var tableView: UITableView!
+    
+    /*
+     1.Implementar
+      DataSource
+     
+     
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //No olvidar
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        // Para interactuar ,implementar el delegate
+        
+        tableView.delegate = self
     }
 
 
 }
+//MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("Seleccionaste la celda \(indexPath.row)")
+    }
+}
+//MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    // 2. Metodo para saber que celdas deben mostrarse.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Celda \(indexPath.row)"
+        
+        return cell
+        
+    }
+    
+    }
+    
+
 
